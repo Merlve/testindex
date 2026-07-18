02821b266251b42492445d4b3e1bfa9c141191f3
Melvin Mmehibe <melvinmmehibe@gmail.com>
2026-07-18 19:25:43 +0100

    refactor: remove legacy patch scripts
    Delete unused CJS patch files used for modifying codebase during development.

--- fix-telegram.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let text = fs.readFileSync('telegram.ts', 'utf-8');
-text = text.replace(/\\`/g, '`');
-text = text.replace(/\\\$/g, '$');
-fs.writeFileSync('telegram.ts', text);
